91e447688a6dead3a94c311ece915e2c70024f2c
Bittu Gupta <rajnish1335@gmail.com>
2025-12-24 22:05:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_qa_rag",
+    version="0.1.0",
+    author="Rajnish Kumar Gupta",
+    packages=find_packages(),
+    install_requires=[]
+)
